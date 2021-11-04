ed905136485aa8464a8ed0112c3ee02ea15b3f5d
Fatih <fatihbuyukguclu@gmail.com>
2021-11-04 17:18:58 +0300

    Adding List Of Owners and Display on Owner Index Page. closes #16

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,15 +1,25 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.service.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
-    @RequestMapping({"","/", "/index", "/index.html"})
-    public String listOwners(){
+    private final OwnerService ownerService;
 
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"","/", "/index", "/index.html"})
+    public String listOwners(Model model){
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
 
     }
