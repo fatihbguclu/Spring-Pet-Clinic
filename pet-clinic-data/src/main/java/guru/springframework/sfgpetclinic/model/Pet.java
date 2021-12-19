5b3cfdf2ad554230b8f017d732fce55f427b2f01
Fatih <fatihbuyukguclu@gmail.com>
2021-12-19 15:41:39 +0300

    New Pet Or Update Pet Added. closes #55

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -16,10 +17,22 @@ import java.util.Set;
 @Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+
+        if (visits == null || visits.size() > 0 ) {
+            this.visits = visits;
+        }
+    }
+
     @Column(name = "name")
     private String name;
 
-    //@Column(name = "pet_type")
     @ManyToOne
     @JoinColumn(name = "type_id")
     private PetType petType;
@@ -29,9 +42,9 @@ public class Pet extends BaseEntity {
     private Owner owner;
 
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
-
 }
